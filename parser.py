bdc12780ccf242c4531ec17660ef0216a404e79b
s_labrinos <slabrinos9214@gmail.com>
2021-04-14 12:44:27 +0300

    parser completed

--- parser.py
+++ parser.py
@@ -1,24 +1,24 @@
 def in_thematic_area(phd_title: str) -> bool:
-    area_keywords = ["information retrieval"]
+    area_keywords = ["information retrieval", "information extraction", "search engine",
+                     "information overload", "text retrieval", "digital libraries",
+                     "information seeking", "retrieval method", "web search",
+                     "retrieval systems", "ir system", "ir model"]
 
-    for keyword in area_keywords:
-        if keyword in phd_title:
-            return True
-
-    return False
+    return any(keyword in phd_title for keyword in area_keywords)
 
 
 try:
-    file = open("./input/sample.xml")
+    file = open("./input/dblp-2021-02-01.xml")
 except FileNotFoundError as e:
     print(f"Error opening the file: {e}")
     exit(-1)
 else:
     year_counts = dict()
     valid = False
+
     for line in file:
         if line.startswith("<title>"):
-            phd_valid = in_thematic_area(line[7:len(line) - 7])
+            phd_valid = in_thematic_area(line[7:len(line) - 9].lower())
 
         if line.startswith("<year>") and phd_valid:
             year = line[6:10]
